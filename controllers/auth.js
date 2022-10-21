d9535d2924a86d21d8e22b1f5cc5db3c4da7c8a3
sonuengineer <prajapatisonu50@gmail.com>
2022-10-21 11:41:05 +0530

    login and sign  page with  hashing algoritham

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,42 @@
 const User = require("../model/user");
-
+const bcrypt = require("bcrypt");
 exports.register = (req, res, next) => {
     const { name, email, password, phone } = req.body;
 
 
-    User.findAll({ where: { email: email } }).then((users) => {
-        const user = users[0];
-        if (user)
-            res.json({
-                success: false,
-                message: "User Already exist. Please Login",
-            });
-        else {
-            User.create({
-                name: name,
-                email: email,
-                password: password,
-                phone: phone,
 
-            })
-                .then(() => {
-                    res.status(200).json({
-                        success: true,
-                        message: "Successfully Signed Up, Login now",
-                    });
-                })
-                .catch((err) => {
-                    console.log(err);
-                    res.json({ success: false, message: "error while registering" });
+    bcrypt.hash(password, 10, (err, hash) => {
+        User.findAll({ where: { email: email } }).then((users) => {
+            const user = users[0];
+            if (user)
+                res.json({
+                    success: false,
+                    message: "User Already exist. Please Login",
                 });
-        }
+            else {
+                User.create({
+                    name: name,
+                    email: email,
+                    password: hash,
+                    //password: password,
+                    phone: phone,
+    
+                })
+                    .then(() => {
+                        res.status(200).json({
+                            success: true,
+                            message: "Successfully Signed Up, Login now",
+                        });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        res.json({ success: false, message: "error while registering" });
+                    });
+            }
+        });
+
     });
+    
 
 };
 
@@ -43,13 +49,12 @@ exports.login = async (req, res, next) => {
                 .status(404)
                 .json({ success: false, message: "user does not exist" });
 
-        var a = user.password
-        var b = req.body.password
-    
-        const passMatch=(a==b)?true:false
+        //var a = user.password
+        //var b = req.body.password
       
-      
-        if (passMatch == false)
+        //const passMatch=(a==b)?true:false
+        const passMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!passMatch)
             return res
                 .status(401)
                 .json({ success: false, message: "Incorrect Password" });
