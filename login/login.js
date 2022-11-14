9f41a9f1f55f1dac1546b7d4976fb42c81d196c4
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-11-14 18:28:18 +0530

    Update login.js

--- login/login.js
+++ login/login.js
@@ -13,12 +13,15 @@ form.addEventListener('submit', (e) => {
     password.value = ''
     axios.post('http://localhost:3000/user/login', obj)
         .then(res => {
-           
+            localStorage.setItem('token', `${res.data.token}`)
+
             alert(res.data.message)
-           
+            window.location.href = '../non-premium-frontend/index.html';
         })
         .catch(err => {
             console.log(err)
             alert(err.response.data.message)
         })
 })
+
+
