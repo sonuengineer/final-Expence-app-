f2e3bbdf328ab49b8c560c6176ba307f1ae1a67e
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-11-17 11:59:58 +0530

    Adding primium section

---
+++ controllers/premium.js
@@ -0,0 +1,42 @@
+const Razorpay = require('razorpay')
+const User = require('../models/user')
+const Order = require('../models/order')
+
+
+exports.premiumOrderGeneration = (req, res, next) => {
+    var instance = new Razorpay({
+        key_id:'rzp_test_ps6lSD7tRRGeh5',
+        key_secret:'43ZgMMjSd2tmkoIT5rs5J3BB'
+    })
+    var options = {
+        amount: req.body.amount,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "order_rcptid_11",
+       
+    };
+    instance.orders.create(options, (err, order) => {
+        if (err) {
+            console.log(err)
+        }
+        else
+            res.status(201).json({ order })
+    })
+}
+
+exports.updateTransactionDetails = (req, res, next) => {
+    req.user.update({ isPremium: true })
+        .then()
+        .catch(err => {
+            console.log(err)
+            return res.json({ error: err })
+        })
+    req.user.createOrder({
+        orderId: req.body.orderId,
+        paymentId: req.body.paymentId
+    })
+        .then()
+        .catch(err => {
+            console.log(err)
+            res.json({ error: err })
+        })
+}
