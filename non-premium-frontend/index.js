70ca47d3e75c047f70b6eee2d4c6e4bb71652f11
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-11-17 12:04:02 +0530

    Add files via upload

--- non-premium-frontend/index.js
+++ non-premium-frontend/index.js
@@ -48,7 +48,7 @@ getExpense.addEventListener('click', () => {
 function displayExpenses(limit, page = 1, rows = localStorage.getItem('rows')) {
     const displayContainer = document.getElementById('displayContainer')
     displayContainer.innerHTML = ''
-    axios.get(`http://localhost:3000/getExpenses`, { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` } })
+    axios.get(`http://localhost:3000/getExpenses?limit=${limit}&page=${page}&rows=${rows}`, { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` } })
         .then(response => {
             console.log(response.data.expenses)
             const table = document.createElement('table')
@@ -79,12 +79,20 @@ function displayExpenses(limit, page = 1, rows = localStorage.getItem('rows')) {
                 total += parseInt(`${expense.amount}`)
                 tbody.appendChild(row);
             })
-            
+            tfoot.innerHTML = `
+                <tr>
+                <td>Total</td>
+                <td>${total}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr>
+            `
             table.appendChild(thead)
             table.appendChild(tbody)
             table.appendChild(tfoot)
             displayContainer.appendChild(table)
-            
+            pagination(response)
 
         })
         .catch(err => {
@@ -98,9 +106,7 @@ const logout = document.getElementById('logout')
 logout.addEventListener('click', () => {
     window.location.href = '../login/login.html'
     localStorage.removeItem('token')
-  
 })
-
 
 
 
@@ -122,11 +128,70 @@ displayContainer.addEventListener('click', (e) => {
 })
 
 
+//premium
+const premiumBtn = document.getElementById("premium");
+const payBtn = document.getElementById('pay')
+const close = document.getElementById("close");
+const container = document.getElementById("popup-container");
+const amount = 49900
+let orderId;
 
 
+premiumBtn.addEventListener("click", () => {
+    container.classList.add("active");
+    axios.post('http://localhost:3000/premium/create/order', { amount: amount }, { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` } })
+        .then(response => {
+            orderId = response.data.order.id;
+            payBtn.style = "display:block"
+        })
+        .catch(err => {
+            console.log(err)
+        })
+});
+
+close.addEventListener("click", () => {
+    container.classList.remove("active");
+    payBtn.style = "display:none"
+});
 
 
+let paymentId;
+let signature;
+payBtn.addEventListener('click', (e) => {
+    container.classList.remove("active");
+    payBtn.style = "display:none"
+    var options = {
+        "key": "rzp_test_ps6lSD7tRRGeh5", // Enter the Key ID generated from the Dashboard
+        "amount": `${amount}`, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "currency": "INR",
+        "name": "Expense Tracker",
+        "description": "Premium",
+        //"image": "https://example.com/your_logo",
+        "order_id": orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "handler": function (response) {
+            paymentId = response.razorpay_payment_id;
+            signature = response.razorpay_signature;
+            alert(`Payment successful: your order ID: ${response.razorpay_order_id} and payment ID:${response.razorpay_payment_id}`);
+            window.location.href = "../premium-frontend/premium.html"
 
-   
+            axios.post('http://localhost:3000/transaction/detail', { orderId: orderId, paymentId: paymentId }, { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` } })
+                .then()
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        "theme": {
+            "color": "#3399cc"
+        }
+    };
+    var rzp1 = new Razorpay(options);
+    rzp1.on('payment.failed', function (response) {
+        alert(response.error.description);
+    });
+    rzp1.open();
+    e.preventDefault();
+
+});
+
 
 
