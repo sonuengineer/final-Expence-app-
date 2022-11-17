ae5615283dc27aa1c8467c2a44b663aeb65fb641
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-11-17 12:03:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ const sequelize = require('./util/database')
 
 const User = require('./models/user')
 const Expense = require('./models/expense')
+const Order = require('./models/order')
+
+
 
 const cors = require('cors')
 
 const authRoutes = require('./routes/auth')
 const expenseRoutes = require('./routes/expense')
+const premiumRoutes = require('./routes/premium')
 
 
 
@@ -30,20 +34,26 @@ app.use(cors());
 
 
 
+
 User.hasMany(Expense);
 Expense.belongsTo(User);
-
+User.hasOne(Order);
+Order.belongsTo(User);
 
 
 app.use('/user', authRoutes);
 app.use(expenseRoutes);
+app.use(premiumRoutes);
+
 
 
 sequelize
     // .sync({ force: true })
     .sync()
     .then(() => {
-      
+        // https
+        //     .createServer({ key: privateKey, cert: certificate }, app)
+        //     .listen(process.env.PORT || 3000)
         app.listen(process.env.PORT || 3000)
     })
     .catch(err => {
