82a2b30d7f1ec57ac9be72f3b48dad533e54a2d6
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-12-07 13:11:14 +0530

    Add files via upload

--- routes/premium.js
+++ routes/premium.js
@@ -7,6 +7,9 @@ const authorization = require('../middleware/authorization')
 const router = express.Router()
 
 router.post('/premium/create/order', authorization, premiumController.premiumOrderGeneration)
-router.post('/transaction/detail', authorization, premiumController.updateTransactionDetails)
+//router.post('/transaction/detail', authorization, premiumController.updateTransactionDetails)
+
+router.get('/premium/leaderboard', authorization, premiumController.getAllUSer)
+
 
 module.exports = router
