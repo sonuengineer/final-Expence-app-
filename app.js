fd8b3501281c302ff2dd4f82474764510258c6fb
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-12-08 16:05:41 +0530

    Updating app.js file for forget password

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const sequelize = require('./util/database')
 const User = require('./models/user')
 const Expense = require('./models/expense')
 const Order = require('./models/order')
+const Password = require('./models/password')
 
 
 
@@ -21,6 +22,7 @@ const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const expenseRoutes = require('./routes/expense')
 const premiumRoutes = require('./routes/premium')
+const forgotPasswordRoutes = require('./routes/forgotPassword')
 
 
 
@@ -39,11 +41,14 @@ User.hasMany(Expense);
 Expense.belongsTo(User);
 User.hasOne(Order);
 Order.belongsTo(User);
+User.hasMany(Password);
+Password.belongsTo(User);
 
 
 app.use('/user', authRoutes);
 app.use(expenseRoutes);
-app.use(premiumRoutes);
+app.use('/purchase',premiumRoutes);
+app.use(forgotPasswordRoutes)
 
 
 
